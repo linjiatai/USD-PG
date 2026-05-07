c067d00c80d34b54021055ddb70919a2ba8f32ce
linjiatai <93721368+linjiatai@users.noreply.github.com>
2026-05-07 15:53:38 +0800

    Add files via upload

---
+++ moco/loader.py
@@ -0,0 +1,47 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import random
+
+from PIL import ImageFilter
+
+
+class ThreeCropsTransform:
+    """Take two random crops of one image as the query and key."""
+
+    def __init__(self, q1_transform, q2_transform, k_transform):
+        self.q1_transform = q1_transform
+        self.q2_transform = q2_transform
+        self.k_transform = k_transform
+
+    def __call__(self, x):
+        q1 = self.q1_transform(x)
+        q2 = self.q2_transform(x)
+        k = self.k_transform(x)
+        return [q1, q2, k]
+    
+class TwoCropsTransform:
+
+
+    def __init__(self, base_transform):
+        self.base_transform = base_transform
+
+
+    def __call__(self, x):
+        q = self.base_transform(x)
+        k = self.base_transform(x)
+        return [q, k]
+
+
+class GaussianBlur:
+    """Gaussian blur augmentation in SimCLR https://arxiv.org/abs/2002.05709"""
+
+    def __init__(self, sigma=[0.1, 2.0]):
+        self.sigma = sigma
+
+    def __call__(self, x):
+        sigma = random.uniform(self.sigma[0], self.sigma[1])
+        x = x.filter(ImageFilter.GaussianBlur(radius=sigma))
+        return x
